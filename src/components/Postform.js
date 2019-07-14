5f54b5d331dcdcba16e41227efe1e0bd49f37633
raudelgar <raudel24@gmail.com>
2019-07-14 11:47:49 -0400

    simple react implementation completed. Next step, implement redux

---
+++ src/components/Postform.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+class Postform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form action="" onSubmit={this.onSubmit}>
+          <div>
+            <label htmlFor="">Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label htmlFor="">Body:</label>
+            <br />
+            <textarea
+              name="body"
+              id=""
+              cols="30"
+              rows="10"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Postform;
