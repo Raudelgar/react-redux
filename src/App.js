d27c41b6c4127502ade44cddeebe808b69053745
raudelgar <raudel24@gmail.com>
2019-07-14 12:20:06 -0400

    adding storeJS, pending rootReducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+
+//Components
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
